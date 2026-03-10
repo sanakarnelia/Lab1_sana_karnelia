ee1b4001ab4ae79087b61ef7ebef078272dedeea
sanakarnelia <sana.karnelia@gmail.com>
2026-03-10 16:22:49 -0400

    Initial Commit

---
+++ Lab1_sana_karnelia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_sana_karnelia
+//
+//  Created by Maaz Karnelia on 2026-03-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_sana_karnelia/Lab1_sana_karneliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_sana_karneliaApp.swift
+//  Lab1_sana_karnelia
+//
+//  Created by Maaz Karnelia on 2026-03-10.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_sana_karneliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
