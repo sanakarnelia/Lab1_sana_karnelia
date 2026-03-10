64d46d95a38f7bc7ab7c6b82bf036f76db2066cb
sanakarnelia <sana.karnelia@gmail.com>
2026-03-10 16:53:06 -0400

    add prime/notprime logicc

--- Lab1_sana_karnelia/GameEngine.swift
+++ Lab1_sana_karnelia/GameEngine.swift
@@ -10,4 +10,22 @@ import Combine
 
 class GameEngine: ObservableObject {
     @Published var currentNumber = Int.random(in: 2...100)
+    
+    
+    //prime/notprime logic
+    private func isPrime(_ n: Int) -> Bool {
+           if n < 2 { return false }
+           if n == 2 { return true }
+           if n % 2 == 0 { return false }
+
+           let limit = Int(Double(n).squareRoot())
+           if limit < 3 { return true }
+
+           var i = 3
+           while i <= limit {
+               if n % i == 0 { return false }
+               i += 2
+           }
+           return true
+       }
 }
