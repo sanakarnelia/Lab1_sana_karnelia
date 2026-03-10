5057794ef7966510368c31d6a375b3894ab697ca
sanakarnelia <sana.karnelia@gmail.com>
2026-03-10 17:05:53 -0400

    male pime/notprime tapable

--- Lab1_sana_karnelia/ContentView.swift
+++ Lab1_sana_karnelia/ContentView.swift
@@ -9,23 +9,51 @@ import SwiftUI
 
 
 struct ContentView: View {
-  
+    
     
     //GameEngine
     @StateObject private var engine = GameEngine()
     var body: some View {
-       
-        Text("\(engine.currentNumber)")
         
-        VStack{
-            Text("Prime")
-            Text("Not Prime")
+        ZStack {
+            Color.white.ignoresSafeArea()
+            
+            VStack(spacing: 26) {
+                
+  
+                // Random number
+                Text("\(engine.currentNumber)")
+                    .font(.system(size: 64, weight: .semibold, design: .serif))
+                    .foregroundColor(.teal)
+                
+                // Tappable labels (Prime / Not Prime)
+                VStack(spacing: 18) {
+                    choiceLabel("Prime") {
+                     print("click - prime")
+                    }
+                    
+                    choiceLabel("not Prime") {
+                       print("click - notprime")
+                    }
+                }
             }
-        .font(.system(size: 34))
-        .foregroundColor(.teal)
+        }
+        
     }
-}
+    //ui helper
+    private func choiceLabel(_ title: String, action: @escaping () -> Void) -> some View {
+          Text(title)
+              .font(.system(size: 34, weight: .regular, design: .serif))
+              .foregroundColor(.teal)
+              .padding(.vertical, 6)
+              .contentShape(Rectangle()) // bigger tap area
+              .onTapGesture {
+                  action()
+              }
+      }
 
+}
+            
 #Preview {
     ContentView()
 }
